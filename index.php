c0032a2a13c6a2322f110579371cb2aba3ebb867
Lauren Holloway <laurenholloway1010@gmail.com>
2015-09-04 02:24:19 -0400

    updated login page

---
+++ index.php
@@ -0,0 +1,47 @@
+<html>
+<head>
+<title>
+    fitKe
+</title>
+    <link href="css/signin.css" rel="stylesheet" type="text/css">
+    <!-- Latest compiled and minified CSS -->
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/css/bootstrap.min.css">
+
+    <!-- Optional theme -->
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/css/bootstrap-theme.min.css">
+
+    <!-- Latest compiled and minified JavaScript -->
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/js/bootstrap.min.js"></script>
+</head>
+<body cz-shortcut-listen="true">
+
+<div class="container">
+
+    <form class="form-signin">
+        <h2 class="form-signin-heading">Login to fitKe</h2>
+        <label for="inputEmail" class="sr-only">Email address</label>
+        <input type="email" id="inputEmail" class="form-control" placeholder="Email address" required="" autofocus="">
+        <label for="inputPassword" class="sr-only">Password</label>
+        <input type="password" id="inputPassword" class="form-control" placeholder="Password" required="">
+        <div class="checkbox">
+            <label>
+                <input type="checkbox" value="remember-me"> Remember me
+            </label>
+        </div>
+        <button class="btn btn-lg btn-primary btn-block" type="submit">Sign in</button>
+    </form>
+
+</div> <!-- /container -->
+
+
+<!-- IE10 viewport hack for Surface/desktop Windows 8 bug -->
+<script src="../../assets/js/ie10-viewport-bug-workaround.js"></script>
+
+
+<script type="text/javascript">(function () {
+        return window.SIG_EXT = {};
+    })()</script>
+
+</body>
+
+</html>
